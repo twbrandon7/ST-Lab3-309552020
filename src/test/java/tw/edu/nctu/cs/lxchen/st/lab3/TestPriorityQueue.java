9696279ad823871b492201b82e555a4ed94595cb
Li-Xian Chen <twbrandon7@gmail.com>
2021-03-18 16:43:36 +0800

    Add first test

---
+++ src/test/java/tw/edu/nctu/cs/lxchen/st/lab3/TestPriorityQueue.java
@@ -0,0 +1,10 @@
+package tw.edu.nctu.cs.lxchen.st.lab3;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPriorityQueue {
+    @Test
+    public void test1() {
+        System.out.println("test");
+    }
+}
